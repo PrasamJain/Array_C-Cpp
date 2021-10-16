4f69f15bb3f7e5d408dfdf9337d1100a6bffc737
prasam jain <90105063+pj1807@users.noreply.github.com>
2021-10-16 13:03:40 +0530

    Update Arrays_delete_end.cpp

--- Arrays_delete_end.cpp
+++ Arrays_delete_end.cpp
@@ -1,6 +1,14 @@
+/* IN THIS LAB WE LEARM HOW TO DELETE AN ELEMENTS FROM END */
 
 
+/* We have an array of 10 elements i.e size = 10,
+*                                 1 2 3 4 5 6 7 8 9 10
+* After deleting the last element from an array
+*                                 1 2 3 4 5 6 7 8 9
+* last element whose index number is size-1 i.e. array[9] is deleted.
+*/
 
+//SOURCE CODE
 
 #include<iostream>
 using namespace std;
@@ -23,3 +31,14 @@ int main()
 	delete[] arr;	//delete dynamically allocated memory.
 	return 0;	
 }
+
+
+
+/* 
+* INPUT :-
+	  10
+	  1 2 3 4 5 6 7 8 9 10
+* OUTPUT :-
+	  1 2 3 4 5 6 7 8 9
+	  
+*/
