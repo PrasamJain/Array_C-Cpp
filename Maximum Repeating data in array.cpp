8749c9f944ee54ba1b8584c98e98c5fcf6e22542
prasam jain <90105063+pj1807@users.noreply.github.com>
2022-05-04 22:22:30 +0530

    Update and rename Q9 max_elemnt.cpp to Maximum Repeating data in array.cpp

--- Maximum Repeating data in array.cpp
+++ Maximum Repeating data in array.cpp
@@ -1,3 +1,11 @@
+/*
+Given an array of size n, the array contains numbers in range from 0 to k-1 where k is a positive integer and k <= n. 
+Find the maximum repeating number in this array. 
+For example: let k be 10 the given array be arr[] = {1, 2, 2, 2, 0, 2, 0, 2, 3, 8, 0, 9, 2, 3}, the maximum repeating number would be 2.
+Expected time complexity is O(n^2).
+*/
+
+
 #include<iostream>
 using namespace std;
 int main()
